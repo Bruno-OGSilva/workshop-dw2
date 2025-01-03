1bdc8c7b5469c14a4e5391b2420c57f0e2b01dff
Bruno Silva <bruno.ogs@gmail.com>
2025-01-02 22:05:27 -0500

    feat: adicionar datamart e staging

---
+++ dw/models/staging/stg_commodities.sql
@@ -0,0 +1,23 @@
+
+with source as(
+    select 
+        "Date",
+        "Close",
+        "simbolo"
+    from 
+        {{ source ('dbsales_glpl', 'commodities') }}
+),
+
+
+
+renamed as (
+
+    select
+        cast("Date" as date) as data,
+        "Close" as valor_fechamento,
+        simbolo
+    from 
+        source
+)
+
+select * from renamed
